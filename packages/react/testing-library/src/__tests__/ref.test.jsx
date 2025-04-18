7aaa7566c6342bac501abe5a2b99242a5a41ccf2
Yiming Li <yimingli.cs@gmail.com>
2025-04-18 21:38:36 +0800

    feat: more ref cases

--- packages/react/testing-library/src/__tests__/ref.test.jsx
+++ packages/react/testing-library/src/__tests__/ref.test.jsx
@@ -5,7 +5,10 @@ import { act } from 'preact/test-utils';
 
 describe('component ref', () => {
   it('basic', async () => {
-    const ref1 = vi.fn();
+    const cleanup = vi.fn();
+    const ref1 = vi.fn(() => {
+      return cleanup;
+    });
     const ref2 = createRef();
     const ref3 = vi.fn();
     const ref4 = createRef();
@@ -82,11 +85,279 @@ describe('component ref', () => {
         "_selectorQuery": {},
       }
     `);
-
+    expect(cleanup).toBeCalledTimes(0);
     act(() => {
       _setShow(false);
     });
+    expect(cleanup).toBeCalledTimes(1);
+    expect(cleanup.mock.calls).toMatchInlineSnapshot(`
+      [
+        [],
+      ]
+    `);
     expect(ref3).toHaveBeenCalledWith(null);
     expect(ref4.current).toBeNull();
+  });
+});
+
+describe('element ref', () => {
+  it('basic', async () => {
+    const ref1 = vi.fn();
+    const ref2 = createRef();
+
+    class Comp extends Component {
+      name = 'comp';
+      render() {
+        return (
+          <view>
+            <view ref={ref1} />
+            <view ref={ref2} />
+          </view>
+        );
+      }
+    }
+    render(<Comp />);
+    expect(elementTree).toMatchInlineSnapshot(`
+      <page>
+        <view>
+          <view
+            has-react-ref="true"
+          />
+          <view
+            has-react-ref="true"
+          />
+        </view>
+      </page>
+    `);
+    expect(ref1.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          NodesRef {
+            "_nodeSelectToken": {
+              "identifier": "2",
+              "type": 2,
+            },
+            "_selectorQuery": {},
+          },
+        ],
+      ]
+    `);
+    expect(ref2.current).toMatchInlineSnapshot(`
+      NodesRef {
+        "_nodeSelectToken": {
+          "identifier": "3",
+          "type": 2,
+        },
+        "_selectorQuery": {},
+      }
+    `);
+  });
+
+  it('insert', async () => {
+    const ref1 = vi.fn();
+    const ref2 = createRef();
+    let _setShow;
+
+    function App() {
+      const [show, setShow] = useState(false);
+      _setShow = setShow;
+      return <Comp show={show} />;
+    }
+    class Comp extends Component {
+      name = 'comp';
+      render() {
+        return this.props.show && (
+          <view>
+            <view ref={ref1} />
+            <view ref={ref2} />
+          </view>
+        );
+      }
+    }
+    render(<App />);
+    expect(elementTree).toMatchInlineSnapshot(`<page />`);
+    expect(ref1.mock.calls).toMatchInlineSnapshot(`[]`);
+    expect(ref2.current).toBeNull();
+    act(() => {
+      _setShow(true);
+    });
+    expect(elementTree).toMatchInlineSnapshot(`
+      <page>
+        <view>
+          <view
+            has-react-ref="true"
+          />
+          <view
+            has-react-ref="true"
+          />
+        </view>
+      </page>
+    `);
+    expect(ref1.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          NodesRef {
+            "_nodeSelectToken": {
+              "identifier": "2",
+              "type": 2,
+            },
+            "_selectorQuery": {},
+          },
+        ],
+      ]
+    `);
+    expect(ref2.current).toMatchInlineSnapshot(`
+      NodesRef {
+        "_nodeSelectToken": {
+          "identifier": "3",
+          "type": 2,
+        },
+        "_selectorQuery": {},
+      }
+    `);
+  });
+
+  it('remove', async () => {
+    const ref1 = vi.fn();
+    const ref2 = createRef();
+    let _setShow;
+
+    function App() {
+      const [show, setShow] = useState(true);
+      _setShow = setShow;
+      return <Comp show={show} />;
+    }
+
+    class Comp extends Component {
+      name = 'comp';
+      render() {
+        return this.props.show && (
+          <view>
+            <view ref={ref1} />
+            <view ref={ref2} />
+          </view>
+        );
+      }
+    }
+    render(<App />);
+    expect(elementTree).toMatchInlineSnapshot(`
+      <page>
+        <view>
+          <view
+            has-react-ref="true"
+          />
+          <view
+            has-react-ref="true"
+          />
+        </view>
+      </page>
+    `);
+    expect(ref1.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          NodesRef {
+            "_nodeSelectToken": {
+              "identifier": "2",
+              "type": 2,
+            },
+            "_selectorQuery": {},
+          },
+        ],
+      ]
+    `);
+    expect(ref2.current).toMatchInlineSnapshot(`
+      NodesRef {
+        "_nodeSelectToken": {
+          "identifier": "3",
+          "type": 2,
+        },
+        "_selectorQuery": {},
+      }
+    `);
+    ref1.mockClear();
+    ref2.current = null;
+    act(() => {
+      _setShow(false);
+    });
+    expect(elementTree).toMatchInlineSnapshot(`<page />`);
+    expect(ref1.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          null,
+        ],
+      ]
+    `);
+    expect(ref2.current).toBeNull();
+  });
+
+  it('remove with cleanup function', async () => {
+    const cleanup = vi.fn();
+    const ref1 = vi.fn(() => {
+      return cleanup;
+    });
+    let _setShow;
+
+    function App() {
+      const [show, setShow] = useState(true);
+      _setShow = setShow;
+      return <Comp show={show} />;
+    }
+
+    class Comp extends Component {
+      name = 'comp';
+      render() {
+        return this.props.show && (
+          <view>
+            <view ref={ref1} />
+          </view>
+        );
+      }
+    }
+    render(<App />);
+    expect(elementTree).toMatchInlineSnapshot(`
+      <page>
+        <view>
+          <view
+            has-react-ref="true"
+          />
+        </view>
+      </page>
+    `);
+    expect(ref1.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          NodesRef {
+            "_nodeSelectToken": {
+              "identifier": "2",
+              "type": 2,
+            },
+            "_selectorQuery": {},
+          },
+        ],
+      ]
+    `);
+    expect(cleanup.mock.calls).toMatchInlineSnapshot(`[]`);
+    act(() => {
+      _setShow(false);
+    });
+    expect(elementTree).toMatchInlineSnapshot(`<page />`);
+    expect(ref1.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          NodesRef {
+            "_nodeSelectToken": {
+              "identifier": "2",
+              "type": 2,
+            },
+            "_selectorQuery": {},
+          },
+        ],
+      ]
+    `);
+    expect(cleanup.mock.calls).toMatchInlineSnapshot(`
+      [
+        [],
+      ]
+    `);
   });
 });
