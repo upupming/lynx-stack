10a0933dd87d8314d827caa4e203e01b5b5ab033
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-05-23 13:57:46 +0800

    test(react/transform): use comments from test (dynamic_import) (#891)
    ## Summary
    
    <!-- Can you explain the reasoning behind implementing this change? What
    problem or issue does this pull request resolve? -->
    
    Following up #888. Uses `t.comments` to make sure that comments are
    correctly tested in `swc_plugin_dynamic_import`.
    
    <!-- It would be helpful if you could provide any relevant context, such
    as GitHub issues or related discussions. -->
    
    ## Checklist
    
    <!--- Check and mark with an "x" -->
    
    - [x] Tests updated (or not required).
    - [ ] Documentation updated (or not required).

--- packages/react/transform/tests/__swc_snapshots__/src/swc_plugin_dynamic_import/mod.rs/should_not_import_lazy.js
+++ packages/react/transform/tests/__swc_snapshots__/src/swc_plugin_dynamic_import/mod.rs/should_not_import_lazy.js
@@ -1,13 +1,13 @@
 (async function() {
-    await import("./index.js");
+    await import(/*webpackChunkName: "./index.js-test"*/ "./index.js");
     await import(`./locales/${name}`);
-    await import("ftp://www/a.js");
-    await import("./index.js", {
+    await import(/*webpackChunkName: "ftp://www/a.js-test"*/ "ftp://www/a.js");
+    await import(/*webpackChunkName: "./index.js-test"*/ "./index.js", {
         with: {
             type: "component"
         }
     });
-    await import("ftp://www/a.js", {
+    await import(/*webpackChunkName: "ftp://www/a.js-test"*/ "ftp://www/a.js", {
         with: {
             type: "component"
         }
--- packages/react/transform/tests/__swc_snapshots__/src/swc_plugin_dynamic_import/mod.rs/should_transform_import_call.js
+++ packages/react/transform/tests/__swc_snapshots__/src/swc_plugin_dynamic_import/mod.rs/should_transform_import_call.js
@@ -1,18 +1,18 @@
 import "@lynx-js/react/experimental/lazy/import";
 import { __dynamicImport } from "@lynx-js/react/internal";
 (async function() {
-    await import("./index.js");
+    await import(/*webpackChunkName: "./index.js-test"*/ "./index.js");
     await import(`./locales/${name}`);
-    await import("ftp://www/a.js");
+    await import(/*webpackChunkName: "ftp://www/a.js-test"*/ "ftp://www/a.js");
     await __dynamicImport("https://www/a.js");
     await __dynamicImport(url);
     await __dynamicImport(url + "?v=1.0");
-    await import("./index.js", {
+    await import(/*webpackChunkName: "./index.js-test"*/ "./index.js", {
         with: {
             type: "component"
         }
     });
-    await import("ftp://www/a.js", {
+    await import(/*webpackChunkName: "ftp://www/a.js-test"*/ "ftp://www/a.js", {
         with: {
             type: "component"
         }
