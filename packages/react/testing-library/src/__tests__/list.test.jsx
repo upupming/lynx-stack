6137fba8c68df24880c9339568ea954dc9990100
Zhiyuan Hong <28915578+hzy@users.noreply.github.com>
2025-06-20 15:39:03 +0800

    fix(react): memory leak because of cycle reference (#1071)
    Different from #1054, this fix avoids disconnecting the
    `SnapshotInstance` that is about to be reused, and avoids incompleteness
    when it is reused in the `<list/>`.
    
    <!--
      Thank you for submitting a pull request!
    
    We appreciate the time and effort you have invested in making these
    changes. Please ensure that you provide enough information to allow
    others to review your pull request.
    
    Upon submission, your pull request will be automatically assigned with
    reviewers.
    
    If you want to learn more about contributing to this project, please
    visit:
    https://github.com/lynx-family/lynx-stack/blob/main/CONTRIBUTING.md.
    -->
    
    ## Summary
    
    <!-- Can you explain the reasoning behind implementing this change? What
    problem or issue does this pull request resolve? -->
    
    <!-- It would be helpful if you could provide any relevant context, such
    as GitHub issues or related discussions. -->
    
    ## Checklist
    
    <!--- Check and mark with an "x" -->
    
    - [x] Tests updated (or not required).
    - [ ] Documentation updated (or not required).
    
    ---------
    
    Signed-off-by: Zhiyuan Hong <28915578+hzy@users.noreply.github.com>
    Co-authored-by: Yiming Li <yimingli.cs@gmail.com>

--- packages/react/testing-library/src/__tests__/list.test.jsx
+++ packages/react/testing-library/src/__tests__/list.test.jsx
@@ -2,6 +2,7 @@ import { describe, expect } from 'vitest';
 import { render } from '..';
 import { useState } from '@lynx-js/react';
 import { __pendingListUpdates } from '../../../runtime/lib/list.js';
+import { act } from 'preact/test-utils';
 
 describe('list', () => {
   it('basic', async () => {
@@ -225,5 +226,323 @@ describe('list', () => {
     `);
     expect(uid2).toMatchInlineSnapshot(`2`);
     expect(uid0).toBe(uid2);
+  });
+  it('should reuse removed list item', async () => {
+    let setListVal;
+    let initListVal = Array(6).fill(0).map((v, i) => i);
+
+    const A = () => {
+      return <text>hello</text>;
+    };
+    const Comp = () => {
+      const [listVal, _setListVal] = useState(initListVal);
+      setListVal = _setListVal;
+      const showMask = true;
+
+      return (
+        <view
+          style={{
+            width: '100vw',
+            height: '100vh',
+          }}
+        >
+          {
+            <list
+              style={{
+                width: '100%',
+                height: '100%',
+              }}
+            >
+              {listVal.map((v) => {
+                return (
+                  <list-item item-key={`${v}`} key={v} full-span>
+                    <view>
+                      {showMask
+                        ? <text>{v}</text>
+                        : null}
+                      {showMask
+                        ? <text>{v}</text>
+                        : null}
+                    </view>
+                    {/* This will generate `__DynamicPartSlot` part for testing the hydration behavior of slot is as expected */}
+                    <view>
+                      <A />
+                    </view>
+                  </list-item>
+                );
+              })}
+            </list>
+          }
+        </view>
+      );
+    };
+
+    const { container } = render(<Comp />);
+    expect(container).toMatchInlineSnapshot(`
+      <page>
+        <view
+          style="width:100vw;height:100vh"
+        >
+          <list
+            style="width:100%;height:100%"
+            update-list-info="[{"insertAction":[{"position":0,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"0","full-span":true},{"position":1,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"1","full-span":true},{"position":2,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"2","full-span":true},{"position":3,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"3","full-span":true},{"position":4,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"4","full-span":true},{"position":5,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"5","full-span":true}],"removeAction":[],"updateAction":[]}]"
+          />
+        </view>
+      </page>
+    `);
+    const list = container.firstChild.firstChild;
+
+    const uid0 = elementTree.enterListItemAtIndex(list, 0);
+    const uid1 = elementTree.enterListItemAtIndex(list, 1);
+    const uid2 = elementTree.enterListItemAtIndex(list, 2);
+    const uid3 = elementTree.enterListItemAtIndex(list, 3);
+
+    const listItem3 = list.children[3];
+    expect(listItem3).toMatchInlineSnapshot(`
+      <list-item
+        full-span="true"
+        item-key="3"
+      >
+        <view>
+          <text>
+            3
+          </text>
+          <text>
+            3
+          </text>
+        </view>
+        <view>
+          <text>
+            hello
+          </text>
+        </view>
+      </list-item>
+    `);
+
+    elementTree.leaveListItem(list, uid0);
+    const uid4 = elementTree.enterListItemAtIndex(list, 4);
+    expect(uid4).toBe(uid0);
+
+    elementTree.leaveListItem(list, uid1);
+    const uid5 = elementTree.enterListItemAtIndex(list, 5);
+    expect(uid5).toBe(uid1);
+
+    // Remove the element 3
+    act(() => {
+      setListVal(initListVal.filter(x => x !== 3));
+    });
+
+    const __CreateElement = vi.spyOn(globalThis, '__CreateElement');
+    const __SetAttribute = vi.spyOn(globalThis, '__SetAttribute');
+    const __FlushElementTree = vi.spyOn(globalThis, '__FlushElementTree');
+
+    // Remove action is generated
+    expect(
+      JSON.parse(list.getAttribute('update-list-info'))[1].removeAction,
+    ).toMatchInlineSnapshot(`
+      [
+        3,
+      ]
+    `);
+    // Reuse the element 3
+    elementTree.leaveListItem(list, uid3);
+    elementTree.enterListItemAtIndex(list, 1);
+
+    expect(__CreateElement).toHaveBeenCalledTimes(0);
+    expect(__SetAttribute).toHaveBeenCalledTimes(4);
+    // The original FiberElement of element 3 is reused for
+    // element 1 now
+    expect(__SetAttribute.mock.calls[0][0]).toBe(listItem3);
+    expect(__SetAttribute.mock.calls[0][0].$$uiSign).toBe(uid3);
+    expect(listItem3).toMatchInlineSnapshot(`
+      <list-item
+        full-span="true"
+        item-key="1"
+      >
+        <view>
+          <text>
+            1
+          </text>
+          <text>
+            1
+          </text>
+        </view>
+        <view>
+          <text>
+            hello
+          </text>
+        </view>
+      </list-item>
+    `);
+
+    expect(__SetAttribute.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          <list-item
+            full-span="true"
+            item-key="1"
+          >
+            <view>
+              <text>
+                1
+              </text>
+              <text>
+                1
+              </text>
+            </view>
+            <view>
+              <text>
+                hello
+              </text>
+            </view>
+          </list-item>,
+          "item-key",
+          "1",
+        ],
+        [
+          <list-item
+            full-span="true"
+            item-key="1"
+          >
+            <view>
+              <text>
+                1
+              </text>
+              <text>
+                1
+              </text>
+            </view>
+            <view>
+              <text>
+                hello
+              </text>
+            </view>
+          </list-item>,
+          "full-span",
+          true,
+        ],
+        [
+          1,
+          "text",
+          1,
+        ],
+        [
+          1,
+          "text",
+          1,
+        ],
+      ]
+    `);
+    expect(__FlushElementTree).toHaveBeenCalledTimes(1);
+    expect(__FlushElementTree.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          <list-item
+            full-span="true"
+            item-key="1"
+          >
+            <view>
+              <text>
+                1
+              </text>
+              <text>
+                1
+              </text>
+            </view>
+            <view>
+              <text>
+                hello
+              </text>
+            </view>
+          </list-item>,
+          {
+            "elementID": 36,
+            "listID": 2,
+            "operationID": undefined,
+            "triggerLayout": true,
+          },
+        ],
+      ]
+    `);
+
+    expect(list).toMatchInlineSnapshot(`
+      <list
+        style="width:100%;height:100%"
+        update-list-info="[{"insertAction":[{"position":0,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"0","full-span":true},{"position":1,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"1","full-span":true},{"position":2,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"2","full-span":true},{"position":3,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"3","full-span":true},{"position":4,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"4","full-span":true},{"position":5,"type":"__Card__:__snapshot_a9e46_test_6","item-key":"5","full-span":true}],"removeAction":[],"updateAction":[]},{"insertAction":[],"removeAction":[3],"updateAction":[]}]"
+      >
+        <list-item
+          full-span="true"
+          item-key="4"
+        >
+          <view>
+            <text>
+              4
+            </text>
+            <text>
+              4
+            </text>
+          </view>
+          <view>
+            <text>
+              hello
+            </text>
+          </view>
+        </list-item>
+        <list-item
+          full-span="true"
+          item-key="5"
+        >
+          <view>
+            <text>
+              5
+            </text>
+            <text>
+              5
+            </text>
+          </view>
+          <view>
+            <text>
+              hello
+            </text>
+          </view>
+        </list-item>
+        <list-item
+          full-span="true"
+          item-key="2"
+        >
+          <view>
+            <text>
+              2
+            </text>
+            <text>
+              2
+            </text>
+          </view>
+          <view>
+            <text>
+              hello
+            </text>
+          </view>
+        </list-item>
+        <list-item
+          full-span="true"
+          item-key="1"
+        >
+          <view>
+            <text>
+              1
+            </text>
+            <text>
+              1
+            </text>
+          </view>
+          <view>
+            <text>
+              hello
+            </text>
+          </view>
+        </list-item>
+      </list>
+    `);
   });
 });
