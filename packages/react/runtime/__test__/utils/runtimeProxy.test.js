533c0a1acf3fadbd5d81da6762f978701a51e0ad
Yradex <11014207+Yradex@users.noreply.github.com>
2025-06-20 16:03:37 +0800

    test(react): add tests to our fake runtimeProxy in testing (#1104)

---
+++ packages/react/runtime/__test__/utils/runtimeProxy.test.js
@@ -0,0 +1,34 @@
+// Copyright 2025 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+import { globalEnvManager } from './envManager.ts';
+
+describe('runtimeProxy', () => {
+  beforeEach(() => {
+    globalEnvManager.resetEnv();
+  });
+
+  it('should dispatch event', () => {
+    const event = vi.fn();
+    lynx.getJSContext().addEventListener('event1', event);
+    globalEnvManager.switchToBackground();
+    lynx.getCoreContext().dispatchEvent({ type: 'event1', data: 'test' });
+    expect(event).toBeCalledWith({ type: 'event1', data: 'test' });
+  });
+
+  it('should not trigger event when context mismatch', () => {
+    const event = vi.fn();
+    lynx.getJSContext().addEventListener('event1', event);
+    lynx.getJSContext().dispatchEvent({ type: 'event1', data: 'test' });
+    expect(event).not.toBeCalled();
+  });
+
+  it('should throw when dispatch event to the same context', () => {
+    expect(() => {
+      lynx.getCoreContext().dispatchEvent({ type: 'event1', data: 'test' });
+    }).toThrow();
+  });
+});
--- packages/react/runtime/__test__/utils/runtimeProxy.ts
+++ packages/react/runtime/__test__/utils/runtimeProxy.ts
@@ -1,5 +1,9 @@
+// Copyright 2025 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
 import { vi } from 'vitest';
-import { globalEnvManager } from './envManager.ts';
+
+import { globalEnvManager } from './envManager.js';
 
 const eventEmitters = {};
 
