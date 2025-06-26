6306857152427c610ca2411b42d151c1ee80ba1e
Yiming Li <yimingli.cs@gmail.com>
2025-06-26 22:47:50 +0800

    fix(testing-library): wrap renderPage in act

--- packages/react/testing-library/src/pure.jsx
+++ packages/react/testing-library/src/pure.jsx
@@ -8,7 +8,6 @@ import { act } from 'preact/test-utils';
 
 import { __root } from '@lynx-js/react/internal';
 
-import { commitToMainThread } from '../../runtime/lib/lifecycle/patch/commit.js';
 import { flushDelayedLifecycleEvents } from '../../runtime/lib/lynx/tt.js';
 import { clearPage } from '../../runtime/lib/snapshot.js';
 
@@ -61,7 +60,9 @@ export function render(
 
   globalThis.lynxTestingEnv.switchToMainThread();
   __root.__jsx = enableMainThread ? compMainThread : null;
-  renderPage();
+  act(() => {
+    renderPage();
+  });
   if (enableBackgroundThread) {
     globalThis.lynxTestingEnv.switchToBackgroundThread();
     act(() => {
