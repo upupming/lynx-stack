da7ffeb38a9468e5242ded0e2105fa1b066044b6
Yiming Li <yimingli.cs@gmail.com>
2025-07-10 19:51:57 +0800

    chore: clean files

--- examples/react-compiler/eslint.config.mjs
+++ examples/react-compiler/eslint.config.mjs
@@ -1,19 +1,12 @@
-// import { fixupConfigRules } from '@eslint/compat';
 import js from '@eslint/js';
-// import preactJsx from 'eslint-config-preact';
 import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import ts from 'typescript-eslint';
-
-// console.log('preactJsx', preactJsx)
 
 export default [
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
   ...ts.configs.recommended,
-  // ...fixupConfigRules([
-  //   preactJsx,
-  // ]),
   {
     plugins: {
       'react-hooks': reactHooks,
