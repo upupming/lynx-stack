48489da4de67ac7f310487e8f45244964d6115f7
Yiming Li <yimingli.cs@gmail.com>
2025-07-22 16:51:33 +0800

    chore(testing-library): remove debug info

--- packages/react/testing-library/src/vitest-global-setup.js
+++ packages/react/testing-library/src/vitest-global-setup.js
@@ -35,9 +35,6 @@ globalThis.onInitWorkletRuntime = () => {
     onInitWorkletRuntime();
   }
 
-  if (process.env.DEBUG) {
-    console.log('initWorkletRuntime');
-  }
   lynx.setTimeout = setTimeout;
   lynx.setInterval = setInterval;
   lynx.clearTimeout = clearTimeout;
@@ -53,9 +50,6 @@ globalThis.onInitWorkletRuntime = () => {
 globalThis.onInjectMainThreadGlobals = (target) => {
   if (onInjectMainThreadGlobals) {
     onInjectMainThreadGlobals(target);
-  }
-  if (process.env.DEBUG) {
-    console.log('onInjectMainThreadGlobals');
   }
 
   snapshotInstanceManager.clear();
@@ -89,9 +83,6 @@ globalThis.onInjectMainThreadGlobals = (target) => {
 globalThis.onInjectBackgroundThreadGlobals = (target) => {
   if (onInjectBackgroundThreadGlobals) {
     onInjectBackgroundThreadGlobals(target);
-  }
-  if (process.env.DEBUG) {
-    console.log('onInjectBackgroundThreadGlobals');
   }
 
   backgroundSnapshotInstanceManager.clear();
@@ -145,9 +136,6 @@ globalThis.onResetLynxTestingEnv = () => {
   if (onResetLynxTestingEnv) {
     onResetLynxTestingEnv();
   }
-  if (process.env.DEBUG) {
-    console.log('onResetLynxTestingEnv');
-  }
 
   flushDelayedLifecycleEvents();
   destroyWorklet();
@@ -161,9 +149,6 @@ globalThis.onSwitchedToMainThread = () => {
   if (onSwitchedToMainThread) {
     onSwitchedToMainThread();
   }
-  if (process.env.DEBUG) {
-    console.log('onSwitchedToMainThread');
-  }
 
   setRoot(globalThis.__root);
   options.document = globalThis._document;
@@ -171,9 +156,6 @@ globalThis.onSwitchedToMainThread = () => {
 globalThis.onSwitchedToBackgroundThread = () => {
   if (onSwitchedToBackgroundThread) {
     onSwitchedToBackgroundThread();
-  }
-  if (process.env.DEBUG) {
-    console.log('onSwitchedToBackgroundThread');
   }
 
   setRoot(globalThis.__root);
