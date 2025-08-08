93db6191cf2b0bd4a631fc69550a819c5b3af45c
Yiming Li <yimingli.cs@gmail.com>
2025-08-08 16:29:15 +0800

    fix(testing): GlobalEventEmitter.emit should receive array

--- packages/testing-library/testing-environment/src/lynx/GlobalEventEmitter.ts
+++ packages/testing-library/testing-environment/src/lynx/GlobalEventEmitter.ts
@@ -1,8 +1,4 @@
-// Copyright 2025 The Lynx Authors. All rights reserved.
-// Licensed under the Apache License Version 2.0 that can be found in the
-// LICENSE file in the root directory of this source tree.
-
-class GlobalEventEmitter {
+export class GlobalEventEmitter {
   listeners: Record<string, Function[]> = {};
   addListener(eventName: string, listener: Function): void {
     this.listeners[eventName] ??= [];
@@ -12,13 +8,17 @@ class GlobalEventEmitter {
     if (!this.listeners[eventName]) {
       return;
     }
-    this.listeners[eventName] = this.listeners[eventName].filter((l) => l !== listener);
+    this.listeners[eventName] = this.listeners[eventName].filter((l) =>
+      l !== listener
+    );
   }
   emit(eventName: string, args: any[]): void {
     if (!this.listeners[eventName]) {
       return;
     }
-    this.listeners[eventName].forEach((listener) => args ? listener(...args) : listener());
+    this.listeners[eventName].forEach((listener) =>
+      args ? listener(...args) : listener()
+    );
   }
   clear(): void {
     this.listeners = {};
@@ -36,14 +36,4 @@ class GlobalEventEmitter {
   toggle(eventName: string, ...data: unknown[]): void {
     this.emit(eventName, data);
   }
-}
-
-const jsModules: {
-  readonly GlobalEventEmitter: GlobalEventEmitter;
-} = {
-  GlobalEventEmitter: new GlobalEventEmitter(),
-} as const;
-
-export function getJSModule<T extends keyof typeof jsModules>(moduleName: T): typeof jsModules[T] {
-  return jsModules[moduleName];
 }
