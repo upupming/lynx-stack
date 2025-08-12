d573ef1c130b159b2c7b55273bdf0108d903ee2d
Yiming Li <yimingli.cs@gmail.com>
2025-08-12 17:20:08 +0800

    fix(testing-library): do not transform css imports

---
+++ .changeset/curvy-dragons-appear.md
@@ -0,0 +1,5 @@
+---
+"@lynx-js/react": patch
+---
+
+fix css transform error in testing library
--- packages/react/testing-library/src/__tests__/css/index.test.jsx
+++ packages/react/testing-library/src/__tests__/css/index.test.jsx
@@ -1,6 +1,10 @@
 import '@testing-library/jest-dom';
 import { describe, expect, vi } from 'vitest';
-import { render, screen } from '..';
+import { render, screen } from '../..';
+
+import './style1.css';
+import './style2.css?common';
+import style3 from './style3.module.css';
 
 describe('CSS', () => {
   it('should render a component with CSS styles object', () => {
@@ -24,6 +28,21 @@ describe('CSS', () => {
     expect(container.firstChild).toMatchInlineSnapshot(`
       <text
         style="color: red; font-size: 20px; flex: 1;"
+      >
+        Hello World
+      </text>
+    `);
+  });
+  it('should render a component with CSS module styles object', () => {
+    expect(style3.baz).toMatchInlineSnapshot(`"_baz_60dbcc"`);
+
+    const TestComponent = () => <text style={style3.baz}>Hello World</text>;
+
+    const { container } = render(<TestComponent />);
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <text
+        style="_baz_60dbcc"
       >
         Hello World
       </text>
