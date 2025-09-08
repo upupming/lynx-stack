7b81e2b54702dc44cc7af7a95cb1e3ba14be4f5c
Yiming Li <liyiming.tech@bytedance.com>
2025-09-08 15:55:15 +0800

    feat: add css chunk split to example

--- examples/react/lynx.config.js
+++ examples/react/lynx.config.js
@@ -5,6 +5,23 @@ import { defineConfig } from '@lynx-js/rspeedy';
 const enableBundleAnalysis = !!process.env['RSPEEDY_BUNDLE_ANALYSIS'];
 
 export default defineConfig({
+  tools: {
+    rspack: {
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            // 注意：这里我们创建一个名为'styles'的cacheGroup来提取CSS
+            styles: {
+              name: 'styles',
+              type: 'css/mini-extract',
+              chunks: 'all',
+              enforce: true,
+            },
+          },
+        }
+      }
+    }
+  },
   plugins: [
     pluginReactLynx(),
     pluginQRCode({
