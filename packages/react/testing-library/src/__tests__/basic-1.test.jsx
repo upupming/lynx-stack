3977e645f216dd816d118ba30b538d58f78e8001
Yiming Li <yimingli.cs@gmail.com>
2025-10-19 18:24:46 +0800

    feat: update snapshot

--- packages/react/testing-library/src/__tests__/basic-1.test.jsx
+++ packages/react/testing-library/src/__tests__/basic-1.test.jsx
@@ -1,16 +1,23 @@
-import { render, fireEvent, act, vi } from '..';
+import { render, fireEvent, act } from '..';
 import { useEffect, useState } from 'preact/hooks';
 import { renderToString } from '../../../runtime/lib/renderToOpcodes';
 import { __page } from '../../../runtime/lib/snapshot';
 import { jsx } from '../../../runtime/jsx-runtime/index.js';
+import { prettyFormatSnapshotPatch } from '../../../runtime/lib/debug/formatPatch';
+import { expect, vi } from 'vitest';
 
 test('basic', async () => {
+  vi.spyOn(lynxTestingEnv.backgroundThread.lynxCoreInject.tt, 'OnLifecycleEvent');
+  const onLifecycleEventCalls = lynxTestingEnv.backgroundThread.lynxCoreInject.tt.OnLifecycleEvent.mock.calls;
+  vi.spyOn(lynx.getNativeApp(), 'callLepusMethod');
+  const callLepusMethodCalls = lynx.getNativeApp().callLepusMethod.mock.calls;
+
   const A = () => {
-    return jsx("et1", {})
-  }
-  
+    return jsx('et1', {});
+  };
+
   let setW, setId1, setId2, setHandle1, setHandle2, setHello0, setHello1, setTextProps;
-  
+
   function Comp() {
     const [w, _setW] = useState('100px');
     const [id1, _setId1] = useState('id1');
@@ -31,29 +38,29 @@ test('basic', async () => {
     setHello0 = _setHello0;
     setHello1 = _setHello1;
     setTextProps = _setTextProps;
-    
+
     return (
       jsx('et2', {
-      values: [
-        `background-color: red; width: ${w};`,
-        id1,
-        handle1,
-        id2,
-        handle2,
-        {
-          ...textProps,
-          __spread: true
-        }
-      ],
-      $0: hello0,
-      $1: /* @__PURE__ */ __vite_ssr_import_0__.jsx(A, {}),
-      $2: hello1
-    })
-    )
+        values: [
+          `background-color: red; width: ${w};`,
+          id1,
+          handle1,
+          id2,
+          handle2,
+          {
+            ...textProps,
+            __spread: true,
+          },
+        ],
+        $0: hello0,
+        $1: /* @__PURE__ */ __vite_ssr_import_0__.jsx(A, {}),
+        $2: hello1,
+      })
+    );
   }
   const { container } = render(<Comp />, {
     enableMainThread: true,
-    enableBackgroundThread: true
+    enableBackgroundThread: true,
   });
   expect(container).toMatchInlineSnapshot(`
     <page>
@@ -83,7 +90,7 @@ test('basic', async () => {
       </view>
     </page>
   `);
-  
+
   act(() => {
     setHello0('hello0-1');
     setHello1('hello1-1');
@@ -120,6 +127,171 @@ test('basic', async () => {
         </text>
       </view>
     </page>
-  `)
-  
+  `);
+
+  onLifecycleEventCalls[0][0][1].root = JSON.stringify(
+    JSON.parse(onLifecycleEventCalls[0][0][1].root),
+    null,
+    2,
+  );
+  expect(onLifecycleEventCalls).toMatchInlineSnapshot(`
+    [
+      [
+        [
+          "rLynxFirstScreen",
+          {
+            "jsReadyEventIdSwap": {},
+            "root": "{
+      "id": -1,
+      "type": "root",
+      "children": [
+        {
+          "id": -2,
+          "type": "et2",
+          "values": [
+            "background-color: red; width: 100px;",
+            "id1",
+            "handle1",
+            "id2",
+            "handle2",
+            {
+              "className": "text",
+              "style": "font-size: 10px;",
+              "__spread": true
+            }
+          ],
+          "children": [
+            {
+              "id": -4,
+              "type": null,
+              "values": [
+                "hello0"
+              ],
+              "__slotIndex": 0
+            },
+            {
+              "id": -3,
+              "type": "et1",
+              "__slotIndex": 1
+            },
+            {
+              "id": -5,
+              "type": null,
+              "values": [
+                "hello1"
+              ],
+              "__slotIndex": 2
+            }
+          ],
+          "__slotIndex": 0
+        }
+      ]
+    }",
+          },
+        ],
+      ],
+    ]
+  `);
+
+  for (const call of callLepusMethodCalls) {
+    const snapshotPatch = JSON.parse(call[1]['data']).patchList[0].snapshotPatch;
+    const formattedSnapshotPatch = prettyFormatSnapshotPatch(snapshotPatch);
+    call[1]['data'] = JSON.stringify(
+      {
+        ...JSON.parse(call[1]['data']),
+        patchList: [{ snapshotPatch: formattedSnapshotPatch }],
+      },
+      null,
+      2,
+    );
+  }
+  expect(callLepusMethodCalls).toMatchInlineSnapshot(`
+    [
+      [
+        "rLynxChange",
+        {
+          "data": "{
+      "patchList": [
+        {
+          "snapshotPatch": [
+            {
+              "op": "SetAttribute",
+              "id": -2,
+              "dynamicPartIndex": 5,
+              "value": {
+                "className": "text",
+                "style": "font-size: 10px;"
+              }
+            }
+          ]
+        }
+      ]
+    }",
+          "patchOptions": {
+            "isHydration": true,
+            "pipelineOptions": {
+              "dsl": "reactLynx",
+              "needTimestamps": true,
+              "pipelineID": "pipelineID",
+              "pipelineOrigin": "reactLynxHydrate",
+              "stage": "hydrate",
+            },
+            "reloadVersion": 0,
+          },
+        },
+        [Function],
+      ],
+      [
+        "rLynxChange",
+        {
+          "data": "{
+      "patchList": [
+        {
+          "snapshotPatch": [
+            {
+              "op": "SetAttribute",
+              "id": -2,
+              "dynamicPartIndex": 0,
+              "value": "background-color: red; width: 200px;"
+            },
+            {
+              "op": "SetAttribute",
+              "id": -2,
+              "dynamicPartIndex": 5,
+              "value": {
+                "className": "text-1",
+                "style": "font-size: 10px; color: red;"
+              }
+            },
+            {
+              "op": "SetAttribute",
+              "id": -4,
+              "dynamicPartIndex": 0,
+              "value": "hello0-1"
+            },
+            {
+              "op": "SetAttribute",
+              "id": -5,
+              "dynamicPartIndex": 0,
+              "value": "hello1-1"
+            }
+          ]
+        }
+      ]
+    }",
+          "patchOptions": {
+            "pipelineOptions": {
+              "dsl": "reactLynx",
+              "needTimestamps": true,
+              "pipelineID": "pipelineID",
+              "pipelineOrigin": "reactLynxHydrate",
+              "stage": "hydrate",
+            },
+            "reloadVersion": 0,
+          },
+        },
+        [Function],
+      ],
+    ]
+  `);
 });
